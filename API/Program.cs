0b85c407d34ed9e4620914c275cd36629f09d50d
DizmoProd <dzozulya3377@gmail.com>
2024-07-22 17:29:15 +0100

    init

---
+++ API/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
